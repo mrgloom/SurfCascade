7ef259a706b86a5252e15201640ed27b8239d7bf
Meng <whenov@gmail.com>
2013-12-26 17:37:42 +0800

    modify daily_branch.bat

--- daily_branch.bat
+++ daily_branch.bat
@@ -1,10 +1,10 @@
-set branch = %date:~6,4%%date:~0,2%%date:~3,2%
+set branch=%date:~6,4%%date:~0,2%%date:~3,2%
 git stash -u
 git checkout -b %branch%
 git stash apply
-git commit -a -m %branch%
-git push github %branch%
+git add .
+git commit -m %branch%
+git push origin %branch%
 git checkout master
 git stash pop
-git branch -d %branch%
-pause
+git branch -D %branch%
