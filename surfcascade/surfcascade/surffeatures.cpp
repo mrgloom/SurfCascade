7eedc420e53e3b0631d5b81cfbcae42965849c77
Meng <whenov@gmail.com>
2014-01-02 10:29:05 +0800

    add surf feature, not normalized

--- surfcascade/surfcascade/surffeatures.cpp
+++ surfcascade/surfcascade/surffeatures.cpp
@@ -10,19 +10,70 @@ CvSURFFeatureParams::CvSURFFeatureParams()
 {
     maxCatCount = 0;
     name = SURFF_NAME;
+    featSize = CvSURFEvaluator::n_Bins * CvSURFEvaluator::n_Cells;
 }
+
+const cv::Size CvSURFEvaluator::shapeSize[3] = { cv::Size(2, 2), cv::Size(1, 4), cv::Size(4, 1) };
 
 void CvSURFEvaluator::init(const CvFeatureParams *_featureParams, int _maxSampleCount, Size _winSize)
 {
     CV_Assert(_maxSampleCount > 0);
-    sum.create((int)_maxSampleCount, (_winSize.width + 1) * (_winSize.height + 1), CV_32SC1);
+    for (int bin = 0; bin < n_Bins; bin++)
+        sum[bin].create((int)_maxSampleCount, (_winSize.width + 1) * (_winSize.height + 1), CV_32FC1);
     CvFeatureEvaluator::init(_featureParams, _maxSampleCount, _winSize);
+}
+
+void CvSURFEvaluator::t2b_filter(const Mat& imgp, Mat& imgd, int bin)
+{
+    int d;
+    for (int y = 1; y < winSize.height + 1; y++) {
+        for (int x = 1; x < winSize.width + 1; x++) {
+            switch (bin) {
+                /*
+                 * 0: |dx| - dx
+                 * 1: |dx| + dx
+                 * 2: |dy| - dy
+                 * 3: |dy| + dy
+                 * 4: |du| - du
+                 * 5: |du| + du
+                 * 6: |dv| - dv
+                 * 7: |dv| + dv
+                 */
+            case 0:
+            case 1:
+                d = -imgp.at<int>(y, x - 1) + imgp.at<int>(y, x + 1);
+                break;
+            case 2:
+            case 3:
+                d = -imgp.at<int>(y - 1, x) + imgp.at<int>(y + 1, x);
+                break;
+            case 4:
+            case 5:
+                d = -imgp.at<int>(y - 1, x - 1) + imgp.at<int>(y + 1, x + 1);
+                break;
+            case 6:
+            case 7:
+                d = -imgp.at<int>(y + 1, x - 1) + imgp.at<int>(y - 1, x + 1);
+                break;
+            }
+            imgd.at<int>(y - 1, x - 1) = abs(d) + d * (bin % 2 ? 1 : -1);
+        }
+    }
 }
 
 void CvSURFEvaluator::setImage(const Mat &img, uchar clsLabel, int idx)
 {
-    CV_DbgAssert(!sum.empty());
+    CV_DbgAssert( !sum[0].empty() );
     CvFeatureEvaluator::setImage(img, clsLabel, idx);
+    Mat imgp(winSize.height + 1, winSize.width + 1, img.type());
+    copyMakeBorder(img, imgp, 1, 1, 1, 1, BORDER_REPLICATE);
+
+    Mat imgd(winSize.height, winSize.width, img.type());
+    for (int i = 0; i < sizeof(sum); i++) {
+        Mat innSum(winSize.height + 1, winSize.width + 1, sum[i].type(), sum[i].ptr<int>((int)idx));
+        t2b_filter(imgp, imgd, i);
+        integral(imgd, innSum);
+    }
 }
 
 void CvSURFEvaluator::writeFeatures(FileStorage &fs, const Mat& featureMap) const
@@ -33,33 +84,27 @@ void CvSURFEvaluator::writeFeatures(FileStorage &fs, const Mat& featureMap) cons
 void CvSURFEvaluator::generateFeatures()
 {
     int offset = winSize.width + 1;
-    for (int x = 0; x < winSize.width; x++)
-        for (int y = 0; y < winSize.height; y++)
-            for (int w = 1; w <= winSize.width / 3; w++)
-                for (int h = 1; h <= winSize.height / 3; h++)
-                    if ((x + 3 * w <= winSize.width) && (y + 3 * h <= winSize.height))
-                        features.push_back(Feature(offset, x, y, w, h));
+    for (int shape = 0; shape < sizeof(shapeSize); shape++)
+    for (int cellEdge = 6; cellEdge <= winSize.width / 2; cellEdge++)
+    for (int y = 0; y < winSize.height; y += step)
+    for (int x = 0; x < winSize.width; x += step)
+        features.push_back(Feature(offset, x, y, shape, cellEdge));
+                
     numFeatures = (int)features.size();
 }
 
-CvSURFEvaluator::Feature::Feature()
+CvSURFEvaluator::Feature::Feature(int offset, int x, int y, int shape, int cellEdge)
 {
-    rect = cvRect(0, 0, 0, 0);
-}
+    for (int h = 0; h < shapeSize[shape].height; h++)
+    for (int w = 0; w < shapeSize[shape].width; w++)
+        rect[h * shapeSize[shape].width + w] = cvRect(x, y, cellEdge, cellEdge); //TODO
 
-CvSURFEvaluator::Feature::Feature(int offset, int x, int y, int _blockWidth, int _blockHeight)
-{
-    Rect tr = rect = cvRect(x, y, _blockWidth, _blockHeight);
-    CV_SUM_OFFSETS(p[0], p[1], p[4], p[5], tr, offset)
-    tr.x += 2 * rect.width;
-    CV_SUM_OFFSETS(p[2], p[3], p[6], p[7], tr, offset)
-    tr.y += 2 * rect.height;
-    CV_SUM_OFFSETS(p[10], p[11], p[14], p[15], tr, offset)
-    tr.x -= 2 * rect.width;
-    CV_SUM_OFFSETS(p[8], p[9], p[12], p[13], tr, offset)
+    for (int i = 0; i < n_Cells; i++) {
+        CV_SUM_OFFSETS(fastRect[i].p0, fastRect[i].p1, fastRect[i].p2, fastRect[i].p3, rect[i], offset);
+    }
 }
 
 void CvSURFEvaluator::Feature::write(FileStorage &fs) const
 {
-    fs << CC_RECT << "[:" << rect.x << rect.y << rect.width << rect.height << "]";
+    //fs << CC_RECT << "[:" << rect.x << rect.y << rect.width << rect.height << "]";
 }
--- surfcascade/surfcascade/surffeatures.h
+++ surfcascade/surfcascade/surffeatures.h
@@ -13,45 +13,58 @@ struct CvSURFFeatureParams : CvFeatureParams
 class CvSURFEvaluator : public CvFeatureEvaluator
 {
 public:
+    static const unsigned step = 4;
+    static const unsigned n_Bins = 8;
+    static const unsigned n_Cells = 4;
+    static const cv::Size shapeSize[3];
+    //static cv::Size shapeSize[3];
+    ////CvSURFEvaluator() {
+    ////    shapeSize[0] = cv::Size(2, 2);
+    //    shapeSize[1] = cv::Size(1, 4);
+    //    shapeSize[2] = cv::Size(4, 1);
+    //}
     virtual ~CvSURFEvaluator() {}
     virtual void init(const CvFeatureParams *_featureParams,
         int _maxSampleCount, cv::Size _winSize);
     virtual void setImage(const cv::Mat& img, uchar clsLabel, int idx);
-    virtual float operator()(int featureIdx, int sampleIdx) const
-    { return (float)features[featureIdx].calc(sum, sampleIdx); }
+    virtual float operator()(int varIdx, int sampleIdx) const;
     virtual void writeFeatures(cv::FileStorage &fs, const cv::Mat& featureMap) const;
 protected:
     virtual void generateFeatures();
+    virtual void t2b_filter(const cv::Mat& imgp, cv::Mat& imgd, int bin);
 
     class Feature
     {
     public:
-        Feature();
-        Feature(int offset, int x, int y, int _block_w, int _block_h);
-        uchar calc(const cv::Mat& _sum, size_t y) const;
+        Feature(int offset, int x, int y, int shape, int cellEdge);
+        float calc(const cv::Mat _sum[], size_t y, int featComponent) const;
         void write(cv::FileStorage &fs) const;
 
-        cv::Rect rect;
-        int p[16];
+        cv::Rect rect[n_Cells];
+
+        struct
+        {
+            int p0, p1, p2, p3;
+        } fastRect[n_Cells];
     };
     std::vector<Feature> features;
 
-    cv::Mat sum;
+    cv::Mat sum[n_Bins]; // 8-bin integral image
 };
 
-inline uchar CvSURFEvaluator::Feature::calc(const cv::Mat &_sum, size_t y) const
+inline float CvSURFEvaluator::operator()(int varIdx, int sampleIdx) const
 {
-    const int* psum = _sum.ptr<int>((int)y);
-    int cval = psum[p[5]] - psum[p[6]] - psum[p[9]] + psum[p[10]];
+    int featureIdx = varIdx / (n_Bins * n_Cells);
+    int componentIdx = varIdx % (n_Bins * n_Cells);
+    return (float)features[featureIdx].calc(sum, sampleIdx, componentIdx);
+}
 
-    return (uchar)((psum[p[0]] - psum[p[1]] - psum[p[4]] + psum[p[5]] >= cval ? 128 : 0) |   // 0
-        (psum[p[1]] - psum[p[2]] - psum[p[5]] + psum[p[6]] >= cval ? 64 : 0) |    // 1
-        (psum[p[2]] - psum[p[3]] - psum[p[6]] + psum[p[7]] >= cval ? 32 : 0) |    // 2
-        (psum[p[6]] - psum[p[7]] - psum[p[10]] + psum[p[11]] >= cval ? 16 : 0) |  // 5
-        (psum[p[10]] - psum[p[11]] - psum[p[14]] + psum[p[15]] >= cval ? 8 : 0) | // 8
-        (psum[p[9]] - psum[p[10]] - psum[p[13]] + psum[p[14]] >= cval ? 4 : 0) |  // 7
-        (psum[p[8]] - psum[p[9]] - psum[p[12]] + psum[p[13]] >= cval ? 2 : 0) |   // 6
-        (psum[p[4]] - psum[p[5]] - psum[p[8]] + psum[p[9]] >= cval ? 1 : 0));     // 3
+inline float CvSURFEvaluator::Feature::calc(const cv::Mat _sum[], size_t y, int featComponent) const
+{
+    int binIdx = featComponent % n_Bins;
+    int cellIdx = featComponent / n_Bins;
+    const float* psum = _sum[binIdx].ptr<float>((int)y);
+    return psum[fastRect[cellIdx].p0] - psum[fastRect[cellIdx].p1] - psum[fastRect[cellIdx].p2] + psum[fastRect[cellIdx].p3];
 }
 
 #endif
