14931f7e91c1ea17c1789ebd64561f7ebd5b94fb
Meng <whenov@gmail.com>
2014-02-13 16:42:24 +0800

    add TrainC() to test SGD using linear regression

--- SurfCascade/SurfCascade/Source/LearningAlgorithms/CascadeClassifier/WeakClassifiers/LogisticRegression.h
+++ SurfCascade/SurfCascade/Source/LearningAlgorithms/CascadeClassifier/WeakClassifiers/LogisticRegression.h
@@ -10,14 +10,15 @@ class LogisticRegression : public WeakClassifier
 {
     vector<double> theta;
     int max_iters = 10000;
-    double alpha = 0.000001;
-    double epsilon = 0.0000001;
+    double alpha = 0.00001;
+    double epsilon = 0.000000001;
 
 public:
     LogisticRegression(int patch_index): WeakClassifier(patch_index) {};
     void Train(vector<vector<double>> X, vector<bool> y);
     double Predict(vector<double> X);
     void Print();
+    void TrainC(vector<vector<double>> X, vector<double> y);
 };
 
 #endif
--- SurfCascade/TestLogisticRegression/TestLogisticRegression.cpp
+++ SurfCascade/TestLogisticRegression/TestLogisticRegression.cpp
@@ -13,29 +13,31 @@ int main()
     //ifstream f("C:/Users/Meng/Documents/default1.csv");
     //ifstream f("C:/Users/Meng/Documents/smarket.csv");
     string line;
-    vector<vector<double>> X;
-    vector<bool> y;
+    //vector<vector<double>> X;
+    //vector<bool> y;
+    vector<vector<double>> X = { { 1, 1 }, { 2, 1 }, { 1, 6 }, { 3, 4 }, { 5, 2 }, { 7, 9 }, { 8, 3 }, { 1.5, 6 }, { 10, 11 } };
+    vector<double> y = { 3, 4, 13, 11, 9, 25, 14, 13.5, 32 };
     double a;
 
-    while (getline(f, line))
-    {
-        X.push_back(vector<double>());
-        stringstream ss(line);
-        while (ss >> a)
-        {
-            if (ss.peek() == ',')
-            {
-                X.back().push_back(a);
-                ss.ignore();
-            }
-            else
-                y.push_back((bool)a);
-        }
-    }
+    //while (getline(f, line))
+    //{
+    //    X.push_back(vector<double>());
+    //    stringstream ss(line);
+    //    while (ss >> a)
+    //    {
+    //        if (ss.peek() == ',')
+    //        {
+    //            X.back().push_back(a);
+    //            ss.ignore();
+    //        }
+    //        else
+    //            y.push_back((bool)a);
+    //    }
+    //}
 
     LogisticRegression lr(0);
 
-    lr.Train(X, y);
+    lr.TrainC(X, y);
     lr.Print();
 
     return 0;
