3628d8944660e8cfeae96ab2f33c0551daa62e1c
Meng <whenov@gmail.com>
2014-02-19 12:36:32 +0800

    set some methods in DenseSURFFeatureExtractor to private

--- SurfCascade/Source/FeatureExtractors/DenseSURFFeatureExtractor.h
+++ SurfCascade/Source/FeatureExtractors/DenseSURFFeatureExtractor.h
@@ -21,15 +21,16 @@ class DenseSURFFeatureExtractor : public FeatureExtractor
     static const int step = 2;
     static const int min_cell_edge = 3;
 
+    void T2bFilter(const Mat& img_padded, Mat& img_filtered, int bin);
+    void GetFeatureRects(int x, int y, Size shape, int cell_edge, Rect rects[]);
+    void CalcFeatureValue(const Mat sums[], Rect rects[], vector<double>& feature);
+    void Normalization(vector<double>& feature);
+
 public:
     static const int dim = n_bins * n_cells;
 
     ~DenseSURFFeatureExtractor();
     void ExtractFeatures(string filename, vector<vector<double>>& features_img);
-    void T2bFilter(const Mat& img_padded, Mat& img_filtered, int bin);
-    void GetFeatureRects(int x, int y, Size shape, int cell_edge, Rect rects[]);
-    void CalcFeatureValue(const Mat sums[], Rect rects[], vector<double>& feature);
-    void Normalization(vector<double>& feature);
 };
 
 #endif
