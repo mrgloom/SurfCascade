f111ccd2391840435a8308816d6a513d810643b2
Meng <whenov@gmail.com>
2014-02-20 11:08:14 +0800

    rename Train project to ObjDetector, remove Detect project

--- ObjDetector/ObjDetector.cpp
+++ ObjDetector/ObjDetector.cpp
@@ -67,5 +67,37 @@ int main(int argc, char *argv[])
 
     cout << "Done." << endl;
 
+
+
+
+    string filepath = "D:/facedata/test.jpg";
+
+    /* calculate integral image */
+    Mat sums[DenseSURFFeatureExtractor::n_bins];
+
+    dense_surf_feature_extractor.IntegralImage(filepath, sums);
+
+    Size img_size(sums[0].cols - 1, sums[0].rows - 1);
+
+    /* scan with variant windows */
+    CascadeClassifier cascade_classifier;
+    // TODO: read trained classifier
+
+    for (Rect win(0, 0, 10, 10); win.width <= img_size.width && win.height <= img_size.height; win.width = int(win.width * 1.1), win.height = int(win.height * 1.1))
+    {
+        for (win.y = 0; win.y + win.height <= img_size.height; win.y += 2)
+            for (win.x = 0; win.x + win.width <= img_size.width; win.x += 2)
+            {
+                vector<vector<double>> features_win;
+
+                dense_surf_feature_extractor.ExtractFeatures(sums, win, features_win);
+                //cascade_classifier.Predict(features_win);
+
+                cout << win;
+            }
+    }
+
+
+
     return 0;
 }
