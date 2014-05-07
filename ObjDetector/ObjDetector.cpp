abe11ece7608de306f6c98efed48827e48936834
Meng <whenov@gmail.com>
2014-05-07 16:11:57 +0800

    add wall time & cpu time measurement

--- ObjDetector/ObjDetector.cpp
+++ ObjDetector/ObjDetector.cpp
@@ -17,6 +17,33 @@ using std::array;
 int get_filepaths(string folder, string wildcard, vector<string>& filepaths);
 void fast_nms(vector<Rect>& rects, vector<double>& scores, double overlap_th);
 
+double get_wall_time(){
+    LARGE_INTEGER time, freq;
+    if (!QueryPerformanceFrequency(&freq)){
+        //  Handle error
+        return 0;
+    }
+    if (!QueryPerformanceCounter(&time)){
+        //  Handle error
+        return 0;
+    }
+    return (double)time.QuadPart / freq.QuadPart;
+}
+double get_cpu_time(){
+    FILETIME a, b, c, d;
+    if (GetProcessTimes(GetCurrentProcess(), &a, &b, &c, &d) != 0){
+        //  Returns total user time.
+        //  Can be tweaked to include kernel times as well.
+        return
+            (double)(d.dwLowDateTime |
+            ((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001;
+    }
+    else{
+        //  Handle error
+        return 0;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -125,6 +152,9 @@ int main(int argc, char *argv[])
 
         ofstream of("C:/Users/Meng/Documents/MATLAB/surf.txt", ios::binary);
 
+        double wall0 = get_wall_time();
+        double cpu0 = get_cpu_time();
+
         for (int j = 0; j < filepaths.size(); j++)
         {
         cout << "Detecting image " << j + 1 << '/' << filepaths.size() << endl;
@@ -200,6 +230,12 @@ int main(int argc, char *argv[])
         wins.clear();
         scores.clear();
         }
+
+        double wall1 = get_wall_time();
+        double cpu1 = get_cpu_time();
+
+        cout << "Wall Time = " << wall1 - wall0 << endl;
+        cout << "CPU Time  = " << cpu1 - cpu0 << endl;
 
         of.close();
     }
